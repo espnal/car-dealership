9e2d413bdb061d78f64d35e1f26f3dbabc8921c1
espnal <78657757+espnal@users.noreply.github.com>
2023-12-15 23:42:18 -0400

    .

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,21 @@
 // import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
-// import CustomFilter from "./CustomFilter";
-// import Footer from "./Footer";
-// import NavBar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import Nav from "./Nav";
 // import ShowMore from "./ShowMore";
-// import SearchBar from "./Searchbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 // import Hero from "./Hero";
 
 export {
 //   Hero,
 //   CarCard,
   CustomButton,
-//   CustomFilter,
-//   Footer,
-//   NavBar,
+  CustomFilter,
+  Footer,
+  Nav,
+  SearchManufacturer,
 //   ShowMore,
-//   SearchBar,
+  SearchBar,
 };
